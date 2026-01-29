4927e0ba37d0575d2796e7d930c91136f78543c5
Anastasis Giakoumatos <anastas.giakoumatos@gmail.com>
2026-01-29 09:17:14 +0200

    build: Increment app version to 1.0.1
    This commit updates the application's version information in `app/build.gradle.kts`.
    
    The `versionCode` is incremented from 2 to 3, and the `versionName` is updated from "1.0.0" to "1.0.1", signaling a new patch release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.agcoding.cartrackingapp"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
