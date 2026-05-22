25ba423db15f8717f79857e177fa991ef7e7e1ae
Anastasis Giakoumatos <anastas.giakoumatos@gmail.com>
2026-05-23 00:02:01 +0300

    build: Bump version to 1.0.13
    This commit increments the application version for a new release.
    
    ### Key Changes:
    
    **1. Versioning (`app/build.gradle.kts`):**
    - Incremented `versionCode` from 13 to 14.
    - Updated `versionName` from "1.0.12" to "1.0.13".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.agcoding.cartrackingapp"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 13
-        versionName = "1.0.12"
+        versionCode = 14
+        versionName = "1.0.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
