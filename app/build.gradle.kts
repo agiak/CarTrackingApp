adb5da4547885c400bcdd41715da77a367fbe17a
Anastasis Giakoumatos <anastas.giakoumatos@gmail.com>
2026-07-29 16:21:23 +0300

    Bump up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.agcoding.cartrackingapp"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 18
-        versionName = "1.1.1"
+        versionCode = 19
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
